3cf2944e3e18b372d08b552e659a1bd66596855c
Pablocsks <pablocsks@gmail.com>
2024-10-20 22:43:11 -0300

    p
    a

--- blogJuegos/apps/posts/models.py
+++ blogJuegos/apps/posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser, User
+
 # Create your models here.
+
+
+
+    
+
+
+
+
+class Posts(models.Model):  # nombreapp_nombreclase
+    titulo = models.CharField(
+        max_length=250, null=False, blank=False, verbose_name="Titulo"
+    )
+    contenido = models.TextField(verbose_name="Contenido")
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    imagen = models.ImageField(upload_to="media/posts", null=True, blank=True)
+
+
+
+
+# class Imagenes(models.Model):
+#     imagen = models.ImageField(upload_to="/media/Posts")
+#     post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+
+
